b95cfb3c56ee294643fa93b14fa03f0b7691ebc0
Firdaus Zawakhir <firdaus@checknow.org>
2020-10-14 22:41:47 +0800

    Updates

---
+++ precache-manifest.6648c0d173c9672a0c79768d8cb1924f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860cd22fece270cdac1d4cd43088ebbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62c7ac1ce43df936b41",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e10131e1587f687f514b",
+    "url": "/static/js/2.b3dc571a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3dc571a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62c7ac1ce43df936b41",
+    "url": "/static/js/main.562dee77.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
