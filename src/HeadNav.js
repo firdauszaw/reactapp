21c132a040ff67a1a2919a56b80527bbbdc4b4f0
Firdaus Zawakhir <firdaus@checknow.org>
2020-10-16 23:04:35 +0800

    commit je la

---
+++ src/HeadNav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header, Icon } from 'semantic-ui-react'
+
+const HeadNav = () => (
+  <Header as='h2' icon>
+    <Icon name='settings' />
+    <Icon name='settings' />
+    Account setting
+    <Header.Subheader>
+     blablabla
+    </Header.Subheader>
+  </Header>
+)
+
+export default HeadNav
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>,
+  </div>,
   document.getElementById('root')
 );
 
