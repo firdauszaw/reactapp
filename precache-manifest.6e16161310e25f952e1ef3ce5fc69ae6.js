7f967a93bcf04e97d23ade0996326520a480dc68
Firdaus Zawakhir <firdaus@checknow.org>
2020-10-18 00:54:30 +0800

    Updates

--- precache-manifest.6e16161310e25f952e1ef3ce5fc69ae6.js
+++ precache-manifest.6e16161310e25f952e1ef3ce5fc69ae6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ac3592669e160e0d9fbf296ab3e639a",
+    "revision": "83a641f75343d47bff5d475344bcba14",
     "url": "/index.html"
   },
   {
-    "revision": "832db603eefad6a15a37",
+    "revision": "015c239bf186ccef30a3",
     "url": "/static/css/main.523f09c5.chunk.css"
   },
   {
-    "revision": "04bcd620d58fb253af68",
-    "url": "/static/js/2.136957c0.chunk.js"
+    "revision": "47de307086d84912820e",
+    "url": "/static/js/2.e193a451.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "/static/js/2.136957c0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e193a451.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "832db603eefad6a15a37",
-    "url": "/static/js/main.7d9128c5.chunk.js"
+    "revision": "015c239bf186ccef30a3",
+    "url": "/static/js/main.b955df50.chunk.js"
   },
   {
     "revision": "bccb43675d339c2b2a73",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24694d81ca19ec0d5db5d06816fd42aa.js"
+  "/precache-manifest.6e16161310e25f952e1ef3ce5fc69ae6.js"
 );
 
 self.addEventListener('message', (event) => {
