beef60e80987f41996e264c425f5157c3af23388
Firdaus Zawakhir <firdaus@checknow.org>
2020-10-18 00:26:35 +0800

    new button addition here

--- src/HeadNav.js
+++ src/HeadNav.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import { Header, Icon } from 'semantic-ui-react'
+import React from 'react';
+import {AppBar, Toolbar, IconButton, Grid} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import MyButton from './StyledComponents';
 
-const HeadNav = () => (
-  <Header as='h2' icon>
-    <Icon name='settings' />
-    <Icon name='settings' />
-    Account setting
-    <Header.Subheader>
-     blablabla
-    </Header.Subheader>
-  </Header>
-)
 
-export default HeadNav
+export default function HeadNav(){
+  return (
+    <AppBar position="static" style={{ background: '#2E3B55' }}>
+      <Toolbar>
+        <Grid
+          justify="space-between" // Add it here :)
+          container 
+          spacing={24}
+        >
+        <Grid item>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <MyButton color="inherit">Login</MyButton>
+        </Grid>
+      </Grid>
+    </Toolbar>
+  </AppBar>);
+
+}
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HeadNav from './HeadNav';
 import * as serviceWorker from './serviceWorker';
-
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <div>
+    <HeadNav />
     <App />
   </div>,
   document.getElementById('root')
